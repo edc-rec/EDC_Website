a6707315efd388918ffc9dd35985f62267ea5d59
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-10-09 12:10:04 +0530

    Created an about us page
    Added year by year description in an timeline format

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app=Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",use_reloader=False)
