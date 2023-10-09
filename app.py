9206231691ab8c3789aae5b579ad70bcc8600187
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-10-09 12:33:42 +0530

    provision to add images
    images now acn be added .. i have used os to do this with the help of flask

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import os
 from flask import *
 from flask import Flask,render_template,request,flash,redirect,url_for,session,session
 
+
+images=os.path.join('static','images')
 app=Flask(__name__)
+app.config['icons'] = images
+timeline2 = os.path.join(app.config['icons'], '2017.jpg')
+
+
 
 @app.route('/')
 def hello():
@@ -9,7 +16,7 @@ def hello():
 
 @app.route('/aboutus')
 def aboutus():
-    return render_template('aboutus.html')
+    return render_template('aboutus.html',img1=timeline2)
 
 
 if __name__ == '__main__':
