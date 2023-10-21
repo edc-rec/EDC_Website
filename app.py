83743dd76e4f4eb768fabf4d5d1ca159e736e8c0
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-10-21 22:36:00 +0530

    restructured the who we are page

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from flask import Flask,render_template,request,flash,redirect,url_for,session,s
 images=os.path.join('static','images')
 app=Flask(__name__)
 app.config['icons'] = images
-timeline2 = os.path.join(app.config['icons'], '2017.jpg')
+t1 = os.path.join(app.config['icons'], 'ECELL LOGO.png')
+t2 = os.path.join(app.config['icons'], 'conclave16.png')
+t3 = os.path.join(app.config['icons'], 'E_week16.png')
+t4 = os.path.join(app.config['icons'], 'e_week18.jpg')
 
 
 
@@ -16,7 +19,7 @@ def hello():
 
 @app.route('/aboutus')
 def aboutus():
-    return render_template('aboutus.html',img1=timeline2)
+    return render_template('aboutus.html',img1=t1,img2=t2,img3=t3,img4=t4)
 
 
 if __name__ == '__main__':
