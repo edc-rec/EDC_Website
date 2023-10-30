dad101628813fead653f9cfd23b2bfd601b3556c
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-10-30 19:48:35 +0530

    Started with the team page
    done with part of the team members

--- app.py
+++ app.py
@@ -14,10 +14,27 @@ t4 = os.path.join(app.config['icons'], 'e_week18.jpg')
 l1 = os.path.join(app.config['icons'], 'lamp.png')
 l2 = os.path.join(app.config['icons'], 'light.png')
 
+tm1 = os.path.join(app.config['icons'], 'conclave16.png')
+tm2 = os.path.join(app.config['icons'], 'conclave16.png')
+tm3 = os.path.join(app.config['icons'], 'conclave16.png')
+tm4 = os.path.join(app.config['icons'], 'conclave16.png')
+tm5 = os.path.join(app.config['icons'], 'conclave16.png')
+tm6 = os.path.join(app.config['icons'], 'conclave16.png')
+tm7 = os.path.join(app.config['icons'], 'conclave16.png')
+tm8 = os.path.join(app.config['icons'], 'conclave16.png')
+tm9 = os.path.join(app.config['icons'], 'conclave16.png')
+tm10 = os.path.join(app.config['icons'], 'conclave16.png')
+tm11 = os.path.join(app.config['icons'], 'conclave16.png')
+tm12 = os.path.join(app.config['icons'], 'conclave16.png')
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html',img1=l1,img2=l2)
+
+
+@app.route('/team')
+def teampage():
+    return render_template('team.html',img1=tm1,img2=tm2,img3=tm3,img4=tm4,img5=tm5,img6=tm6,img7=tm7,img8=tm8,img9=tm9,img10=tm10,img11=tm11,img12=tm12)
 
 @app.route('/aboutus')
 def aboutus():
