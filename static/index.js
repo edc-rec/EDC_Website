eeb08dba9f7fe3ad14c4c5a749418c49287b0f73
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2024-02-23 22:15:24 +0530

    update

--- static/index.js
+++ static/index.js
@@ -7,3 +7,12 @@ if (image.style.display == 'none') {
     image.style.display = 'none';
 }
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}  
